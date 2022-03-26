94c8faa8185538a0dfdb915d4047e513e6754258
Joe Rankin <jrankin312@gmail.com>
2022-03-26 11:05:59 -0500

    Fixed config group - removed dashes.

--- src/main/java/com/datbear/GuardiansOfTheRiftHelperConfig.java
+++ src/main/java/com/datbear/GuardiansOfTheRiftHelperConfig.java
@@ -6,11 +6,10 @@ import net.runelite.client.config.ConfigItem;
 
 import java.awt.*;
 
-@ConfigGroup("guardians-of-the-rift-helper")
+@ConfigGroup("guardiansOfTheRiftHelper")
 public interface GuardiansOfTheRiftHelperConfig extends Config
 {
     @ConfigItem(
-            position = 0,
             keyName = "showOverlay",
             name = "Show Overlay",
             description = "Toggles the status overlay"
@@ -21,7 +20,6 @@ public interface GuardiansOfTheRiftHelperConfig extends Config
     }
 
     @ConfigItem(
-            position = 1,
             keyName = "portalSpawn",
             name = "Notify on portal spawn",
             description = "Notifies you when a portal spawns"
@@ -32,7 +30,6 @@ public interface GuardiansOfTheRiftHelperConfig extends Config
     }
 
     @ConfigItem(
-            position = 2,
             keyName = "outlineCellTable",
             name = "Outline cell table",
             description = "Outlines the Cell table when you have no cells remaining"
@@ -43,7 +40,6 @@ public interface GuardiansOfTheRiftHelperConfig extends Config
     }
 
     @ConfigItem(
-            position = 3,
             keyName = "outlineGreatGuardian",
             name = "Outline Great Guardian",
             description = "Outlines the Great Guardian when you have elemental or catalytic essence in your inventory."
@@ -56,7 +52,6 @@ public interface GuardiansOfTheRiftHelperConfig extends Config
 
 
     @ConfigItem(
-            position = 4,
             keyName = "elementalGuardianColor",
             name = "Elemental Outline",
             description = "Color of the outline on the active elemental guardian"
@@ -67,7 +62,6 @@ public interface GuardiansOfTheRiftHelperConfig extends Config
     }
 
     @ConfigItem(
-            position = 5,
             keyName = "catalyticGuardianColor",
             name = "Catalytic Outline",
             description = "Color of the outline on the active catalytic guardian"
