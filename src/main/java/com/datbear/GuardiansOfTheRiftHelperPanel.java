20f05aeebf4ccf4897dfc0858a3f786c4d3b215e
Joe Rankin <jrankin312@gmail.com>
2024-11-17 15:35:26 -0600

    Fixed negative time to start, negative reward points, portal timer overlay when game is over

--- src/main/java/com/datbear/GuardiansOfTheRiftHelperPanel.java
+++ src/main/java/com/datbear/GuardiansOfTheRiftHelperPanel.java
@@ -21,7 +21,7 @@ public class GuardiansOfTheRiftHelperPanel extends OverlayPanel {
     private GuardiansOfTheRiftHelperConfig config;
 
     @Inject
-    public GuardiansOfTheRiftHelperPanel(Client client, GuardiansOfTheRiftHelperPlugin plugin, GuardiansOfTheRiftHelperConfig config){
+    public GuardiansOfTheRiftHelperPanel(Client client, GuardiansOfTheRiftHelperPlugin plugin, GuardiansOfTheRiftHelperConfig config) {
         super(plugin);
         this.client = client;
         this.plugin = plugin;
@@ -32,57 +32,56 @@ public class GuardiansOfTheRiftHelperPanel extends OverlayPanel {
 
 
     @Override
-    public Dimension render(Graphics2D graphics)
-    {
-        if((!plugin.isInMainRegion() && !plugin.isInMinigame()) || !config.showPointsOverlay()){
+    public Dimension render(Graphics2D graphics) {
+        if ((!plugin.isInMainRegion() && !plugin.isInMinigame()) || !config.showPointsOverlay()) {
             return null;
         }
 
-		Optional<Instant> gameStart = plugin.getNextGameStart();
-		if(gameStart.isPresent()) {
-			if(config.startTimerOverlayLocation() == TimerOverlayLocation.InfoBox || config.startTimerOverlayLocation() == TimerOverlayLocation.Both){
-				int timeToStart = ((int)ChronoUnit.SECONDS.between(Instant.now(), gameStart.get()));
-				panelComponent.getChildren().add(LineComponent.builder()
-						.left("Time to start:")
-						.right(""+timeToStart)
-						.build());
-			}
-		} else {
-			if(config.inactivePortalOverlayLocation() == TimerOverlayLocation.InfoBox || config.inactivePortalOverlayLocation() == TimerOverlayLocation.Both){
-				Optional<Instant> despawn = plugin.getLastPortalDespawnTime();
-				int timeSincePortal = despawn.isPresent() ? ((int)(ChronoUnit.SECONDS.between(despawn.get(), Instant.now()))) : 0;
-				panelComponent.getChildren().add(LineComponent.builder()
-						.left("Time since portal:")
-						.right(""+timeSincePortal)
-						.rightColor(plugin.getTimeSincePortalColor(timeSincePortal))
-						.build());
-			}
-		}
+        Optional<Instant> gameStart = plugin.getNextGameStart();
+        if (gameStart.isPresent()) {
+            if (config.startTimerOverlayLocation() == TimerOverlayLocation.InfoBox || config.startTimerOverlayLocation() == TimerOverlayLocation.Both) {
+                int timeToStart = ((int) ChronoUnit.SECONDS.between(Instant.now(), gameStart.get()));
+                if (timeToStart >= 0) {
+                    panelComponent.getChildren().add(LineComponent.builder()
+                            .left("Time to start:")
+                            .right("" + timeToStart)
+                            .build());
+                }
+            }
+        } else {
+            if (config.inactivePortalOverlayLocation() == TimerOverlayLocation.InfoBox || config.inactivePortalOverlayLocation() == TimerOverlayLocation.Both) {
+                Optional<Instant> despawn = plugin.getLastPortalDespawnTime();
+                int timeSincePortal = despawn.isPresent() ? ((int) (ChronoUnit.SECONDS.between(despawn.get(), Instant.now()))) : 0;
+                panelComponent.getChildren().add(LineComponent.builder()
+                        .left("Time since portal:")
+                        .right("" + timeSincePortal)
+                        .rightColor(plugin.getTimeSincePortalColor(timeSincePortal))
+                        .build());
+            }
+        }
 
         panelComponent.getChildren().add(LineComponent.builder()
-                        .left("Reward points:")
-                        .right(plugin.getElementalRewardPoints() + "/" + plugin.getCatalyticRewardPoints())
+                .left("Reward points:")
+                .right(plugin.getElementalRewardPoints() + "/" + plugin.getCatalyticRewardPoints())
                 .build());
 
-		if (config.potentialPoints())
-		{
-			final int potElementalPoints = plugin.potentialPointsElemental();
-			final int potCatalyticPoints = plugin.potentialPointsCatalytic();
-			final int elementalRemain = plugin.getCurrentElementalRewardPoints() % 100;
-			final int catalyticRemain = plugin.getCurrentCatalyticRewardPoints() % 100;
-			final String potPoints = String.format("%d.%02d/%d.%02d", potElementalPoints, elementalRemain, potCatalyticPoints, catalyticRemain);
-			Color potColor = Color.WHITE;
-			if (config.highlightPotential())
-			{
-				potColor = potElementalPoints == potCatalyticPoints ? config.potentialBalanceColor() : config.potentialUnbalanceColor();
-			}
-			panelComponent.getChildren().add(LineComponent.builder()
-				.left("Potential:")
-				.rightColor(potColor)
-				.right(potPoints)
-				.build());
-		}
-		return super.render(graphics);
+        if (config.potentialPoints()) {
+            final int potElementalPoints = plugin.potentialPointsElemental();
+            final int potCatalyticPoints = plugin.potentialPointsCatalytic();
+            final int elementalRemain = plugin.getCurrentElementalRewardPoints() % 100;
+            final int catalyticRemain = plugin.getCurrentCatalyticRewardPoints() % 100;
+            final String potPoints = String.format("%d.%02d/%d.%02d", potElementalPoints, elementalRemain, potCatalyticPoints, catalyticRemain);
+            Color potColor = Color.WHITE;
+            if (config.highlightPotential()) {
+                potColor = potElementalPoints == potCatalyticPoints ? config.potentialBalanceColor() : config.potentialUnbalanceColor();
+            }
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Potential:")
+                    .rightColor(potColor)
+                    .right(potPoints)
+                    .build());
+        }
+        return super.render(graphics);
     }
 
 }
